d23af787acf25c1f66d13f054b29b67720c5012f
Eduardo Miguel Muzo Cansigña <74844624+Miguel-EMC@users.noreply.github.com>
2025-06-25 21:43:42 -0500

    Fix ThemeService instantiation error

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {ThemeService} from "./app/services/theme.service";
-
-const themeService = new ThemeService();
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));

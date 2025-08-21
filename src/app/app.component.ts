c4911e863a68c85314a70f9fe8185cae80257c15
Miguel-EMC <emc.muzo@gmail.com>
2025-08-20 23:18:24 -0500

    aadd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
+import { LanguageToggleComponent } from './shared/components/ui/language-toggle/language-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LanguageToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'protafolio-personal';
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private translate: TranslateService
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.translate.addLangs(['en', 'es']);
+      this.translate.setDefaultLang('es');
+      
+      const browserLang = this.translate.getBrowserLang();
+      const savedLang = localStorage.getItem('portfolio-language');
+      
+      if (savedLang && ['en', 'es'].includes(savedLang)) {
+        this.translate.use(savedLang);
+      } else if (browserLang && browserLang.match(/en|es/)) {
+        this.translate.use(browserLang);
+      } else {
+        this.translate.use('es');
+      }
+    }
+  }
+
+  ngOnInit(): void {
+    // Initialize translation service
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes, withInMemoryScrolling({
+      scrollPositionRestoration: 'top'
+    })), 
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        fallbackLang: 'es'
+      })
+    )
+  ]
 };
